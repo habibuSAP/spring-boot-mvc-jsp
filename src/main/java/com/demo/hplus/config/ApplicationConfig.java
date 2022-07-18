ae7b4a0511d7c58d9f9a0ba32900d0b1830e2c9e
Habibu Abdullahi <habibu.abdullahi@sap.com>
2022-07-18 13:32:42 +0200

    Add: View resolvers xml, bundle, and internal resource resolvers

--- src/main/java/com/demo/hplus/config/ApplicationConfig.java
+++ src/main/java/com/demo/hplus/config/ApplicationConfig.java
@@ -1,9 +1,11 @@
 package com.demo.hplus.config;
 
 import com.demo.hplus.converters.StringToEnumConverter;
+import com.sun.xml.bind.v2.runtime.unmarshaller.XmlVisitor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -12,6 +14,11 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.ResourceBundleViewResolver;
+import org.springframework.web.servlet.view.XmlViewResolver;
+
+import javax.sql.rowset.spi.XmlReader;
+import java.util.ResourceBundle;
 
 @Configuration
 @ComponentScan(basePackages = "com.demo.hplus")
@@ -22,16 +29,32 @@ public class ApplicationConfig extends WebMvcConfigurationSupport {
         resourceHandlerRegistry.addResourceHandler("css/**", "images/**")
                 .addResourceLocations("classpath:/static/css/", "classpath:/static/images/");
     }
+    /*Internal resource view resolver with order priority 2*/
     @Bean
     public InternalResourceViewResolver jspViewResolver(){
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setPrefix("/WEB-INF/jsp/");
         viewResolver.setSuffix(".jsp");
         viewResolver.setViewClass(JstlView.class);
-
+        viewResolver.setOrder(2);
         return viewResolver;
     }
+    /* View resolver using Xml with order priority 1*/
+    @Bean
+    public XmlViewResolver xmlViewResolver(){
 
+        XmlViewResolver viewResolver = new XmlViewResolver();
+        viewResolver.setLocation(new ClassPathResource("views.xml"));
+        viewResolver.setOrder(1);
+        return viewResolver;
+    }
+    /*View resolver using resource bundle*/
+  /*  @Bean
+    public ResourceBundleViewResolver resourceBundleViewResolver(){
+        ResourceBundleViewResolver viewResolver = new ResourceBundleViewResolver();
+        viewResolver.setBasename("views");
+        return viewResolver;
+    }*/
     @Override
     protected void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new StringToEnumConverter());
